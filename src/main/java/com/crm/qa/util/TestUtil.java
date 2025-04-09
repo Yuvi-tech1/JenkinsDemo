9a67a956f9d3fb0c942bb75c3d66da37619afbfd
Yuvi <Yuvi1591@gmail.com>
2025-04-09 18:57:43 +0530

    testdata path

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -21,8 +21,8 @@ public class TestUtil extends TestBase {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "D:\software install\jenkins\gitcode\PageObjectModel\src\main\java\com\crm\qa\testdata\FreeCrmTestData.xlsx";
-	///Users/Public/Documents/workspace"
+	//public static String TESTDATA_SHEET_PATH = "D:\software install\jenkins\gitcode\PageObjectModel\src\main\java\com\crm\qa\testdata\FreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "Users/Public/Documents/FreeCrmTestData.xlsx";
 	//		+ "/FreeCRMTest/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
 
 	static Workbook book;
